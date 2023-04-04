4817914c99cde2572ec1d1fe2d16c8f6b4c23889
Daniel Costa <git@danielcosta.pt>
2023-04-04 23:31:58 +0100

    update dependency

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 
 	"github.com/daco-tech/terraform-provider-sendgrid/sendgrid"
-	"github.com/hashicorp/terraform-plugin-sdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 // Generate the Terraform provider documentation using `tfplugindocs`:
